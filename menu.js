fe8d8f33b92d33c5570b8308ab2f11674a64af0d
andres <herrera8@pdx.edu>
2022-05-30 16:13:02 -0700

    done

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector(".nav-menu");
+const menuLinks = document.querySelector(".nav-items");
 
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
